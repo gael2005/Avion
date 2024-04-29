85b583653fa6ae84793b0051046f2ad9871170fb
Gaël Salaün-Grenier <spaciauxgaming2005@gmail.com>
2024-04-29 14:35:14 -0400

    classe passager

---
+++ Avion/AvionLibrary/Passager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvionLibrary
+{
+    internal class Passager
+    {
+        private string _nom;
+        private string _coordonnée;
+
+        public Passager(string nom, string coordonnée)
+        {
+            Nom = nom;
+            Coordonnée = coordonnée;
+        }
+
+        public string Nom 
+        { 
+            get => _nom; 
+            set
+            {
+                if(value is null) throw new ArgumentNullException("le nom est null");
+                if (value.Length == 0) throw new ArgumentException("le nom est vide");
+
+                _nom = value;
+            }
+        }
+        public string Coordonnée 
+        { 
+            get => _coordonnée; 
+            set
+            {
+                if (value is null) throw new ArgumentNullException("le nom est null");
+                if (value.Length == 0) throw new ArgumentException("le nom est vide");
+                _coordonnée = value;
+            }
+        }
+    }
+}
